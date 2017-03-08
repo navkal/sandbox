e4adfbc9a8e2cb4c1e6d07592bbecb72b1a2ee25
Naomi Navkal <navkal@hotmail.com>
2017-03-08 12:53:21 -0500

    Initial

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
+
+  <?php
+    require_once $_SERVER["DOCUMENT_ROOT"]."/../common/util.php";
+  ?>
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
--- plugin.php
+++ plugin.php
@@ -3,5 +3,11 @@
 <div class="container">
   <?php
     echo( "<h2>Plugin stub</h2>" );
+    
+    // Execute Python script
+    $command = quote( getenv( "PYTHON" ) ) . " test.py";
+    error_log( "===> command=" . $command );
+    exec( $command, $output, $status );
+    echo( "===> output=" . print_r( $output, true ) );
   ?>
 </div>
