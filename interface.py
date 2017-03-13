581b7cd683c73194dd85d24ffadb495cd3634713
Naomi Navkal <navkal@hotmail.com>
2017-03-12 23:16:26 -0400

    Initial

--- interface.py
+++ interface.py
@@ -1,8 +1,9 @@
-import printctl
+import json
 
+import printctl
 printctl.off()
 import queries
 printctl.on()
 
-z = queries.device(3)
-z.properties()
+dev = queries.device(3)
+print( json.dumps( dev.__dict__ ) )
--- plugin.php
+++ plugin.php
@@ -1,12 +1,33 @@
 <!-- Copyright 2016 Energize Apps.  All rights reserved. -->
+<?php
+  // Execute Python script
+  $command = quote( getenv( "PYTHON" ) ) . " interface.py 2>&1";
+  error_log( "===> command=" . $command );
+  exec( $command, $output, $status );
+  error_log( "===> output=" . print_r( $output, true ) );
+
+  // Decode JSON output
+  $dev = json_decode( $output[0] );
+?>
 
 <div class="container">
-  <?php   
-    // Execute Python script
-    $command = quote( getenv( "PYTHON" ) ) . " interface.py 2>&1";
-    error_log( "===> command=" . $command );
-    exec( $command, $output, $status );
-    
-    var_dump( $output );
-  ?>
+  <table class="table" >
+    <thead>
+      <tr>
+        <th>Key</th>
+        <th>Value</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+        foreach ( $dev as $key => $val )
+        {
+          echo( '<tr>' );
+          echo( '<td>' . $key . '</td>' );
+          echo( '<td>' . $val . '</td>' );
+          echo( '</tr>' );
+        }
+      ?>
+    </tbody>
+  </table>
 </div>
