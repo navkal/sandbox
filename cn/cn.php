bd8d1c656893dd1f47774075095e0960993ed7d1
Naomi Navkal <navkal@hotmail.com>
2017-03-17 13:42:08 -0400

    Prototype

--- cn/cn.php
+++ cn/cn.php
@@ -1,13 +1,74 @@
 <!-- Copyright 2017 Energize Apps.  All rights reserved. -->
 
 <script>
-  $( document ).ready( showRoot );
 
-  function showRoot()
+  var TREE = {};
+
+  $( document ).ready( walkTree );
+
+  function walkTree()
   {
+    walkSubtree( "" );
   }
-</script>
+
+  function walkSubtree( path )
+  {
+    // Post request to server
+    var tPostData = new FormData();
+    tPostData.append( "objectType", "circuit" );
+    tPostData.append( "objectSelector", path );
+
+    $.ajax(
+      "ld/query.php",
+      {
+        type: 'POST',
+        processData: false,
+        contentType: false,
+        dataType : 'json',
+        data: tPostData
+      }
+    )
+    .done( handlePostResponse )
+    .fail( handlePostError );
+  }
+
+  function handlePostResponse( tRsp, sStatus, tJqXhr )
+  {
+    // Append to tree
+    var nDepth = tRsp.path.split( "." ).length
+    var sIndent = Array( nDepth ).join( "-")
+    TREE[tRsp.path] = sIndent + tRsp.path + "<br/>";
+
+    // Optionally update display
+    if ( tRsp.children.length == 0 )
+    {
+      $( "#objectTree" ).html( "" );
+      var aPaths = Object.keys( TREE ).sort();
+      for ( var iPath = 0; iPath < aPaths.length; iPath ++ )
+      {
+        $( "#objectTree" ).append( TREE[aPaths[iPath]] );
+      }
+    }
+
+    // Traverse children
+    for ( var iChild = 0; iChild < tRsp.children.length; iChild ++ )
+    {
+      var sChildPath = tRsp.children[iChild][0];
+      if ( sChildPath != tRsp.path )
+      {
+        walkSubtree( sChildPath );
+      }
+    }
+  }
+
+  function handlePostError( tJqXhr, sStatus, sErrorThrown )
+  {
+    console.log( sStatus + " " + sErrorThrown );
+  }
+
+  </script>
 
 <div class="container">
-  <h1>stub</h1>
+  <div id="objectTree" style="overflow:auto">
+  </div>
 </div>
