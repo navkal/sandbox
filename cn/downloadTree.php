4653849d137eb551f670b16c7d8f01e3c501c69d
Naomi Navkal <navkal@hotmail.com>
2017-03-23 11:44:34 -0400

    Prototype

--- cn/downloadTree.php
+++ cn/downloadTree.php
@@ -3,12 +3,15 @@
 
   require_once $_SERVER["DOCUMENT_ROOT"]."/../common/util.php";
 
-  $g_iTestDepth = 2;
-  error_log( "===> Starting tree dump to depth: " . ( $g_iTestDepth ? $g_iTestDepth : "Full" ) );
+  $g_iTestDepth = 0;
+  $sMsgDepth = " tree dump to depth: " . ( $g_iTestDepth ? $g_iTestDepth : "Full" );
+  error_log( "===> Starting" . $sMsgDepth );
 
   $g_aTree = [];
   walkSubtree( "" );
   downloadTree();
+
+  error_log( "===> Completed" . $sMsgDepth );
 
   function walkSubtree( $sPath )
   {
@@ -38,7 +41,6 @@
     $aDevices = $tObject->devices;
     foreach( $aDevices as $aDevice )
     {
-
       $iDeviceId = $aDevice[0];
       $iDeviceLoc = $aDevice[1];
       $sDeviceDescr = $aDevice[2];
@@ -73,13 +75,82 @@
   function writeTree( $file )
   {
     global $g_aTree;
-    ksort( $g_aTree );
+    uksort( $g_aTree, "compareKeys" );
 
     foreach ( $g_aTree as $sKey => $sPath )
     {
-      error_log( "==> key=" . $sKey . " path=" . $sPath );
+      // error_log( "==> key=" . $sKey . " path=" . $sPath );
       fwrite( $file, $g_aTree[$sKey] );
     }
+  }
+
+  function compareKeys( $sKey1, $sKey2 )
+  {
+    // Extract path and device strings
+    $aSplit1 = explode( ' ', $sKey1 );
+    $aSplit2 = explode( ' ', $sKey2 );
+    $sPath1 = array_shift( $aSplit1 );
+    $sPath2 = array_shift( $aSplit2 );
+    $sDev1 = implode( ' ', $aSplit1 );
+    $sDev2 = implode( ' ', $aSplit2 );
+
+    // Split path into fragments
+    $aPath1 = explode( '.', $sPath1 );
+    $aPath2 = explode( '.', $sPath2 );
+
+    // Compare paths
+    $iResult = 0;
+    for ( $iFragment = 0; ( $iResult == 0 ) && ( $iFragment < count( $aPath1 ) ) && ( $iFragment < count( $aPath2 ) ); $iFragment++ )
+    {
+      $sFragment1 = $aPath1[$iFragment];
+      $sFragment2 = $aPath2[$iFragment];
+
+      $bNum1 = ctype_digit( $sFragment1 );
+      $bNum2 = ctype_digit( $sFragment2 );
+
+      if ( $bNum1 && $bNum2 )
+      {
+        // Compare fragments as numbers
+        $iResult = $sFragment1 - $sFragment2;
+      }
+      else
+      {
+        // Compare fragments as strings
+        $iResult = strcmp( $sFragment1, $sFragment2 );
+      }
+    }
+
+    // If leading parts of paths are the same, compare their lengths
+    if ( $iResult == 0 )
+    {
+      $iResult = count( $aPath1 ) - count( $aPath2 );
+    }
+
+    // If paths are the same, compare devices
+    if ( $iResult == 0 )
+    {
+      if ( $sDev1 == $sDev2 )
+      {
+        $iResult = 0;
+      }
+      else if ( $sDev1 == "" )
+      {
+        $iResult = -1;
+      }
+      else if ( $sDev2 == "" )
+      {
+        $iResult = 1;
+      }
+      else
+      {
+        // Compare device IDs, which are guaranteed to be numeric and unique
+        $aDev1 = explode( ',', $sDev1 );
+        $aDev2 = explode( ',', $sDev2 );
+        $iResult = $aDev1[0]  - $aDev2[0];
+      }
+    }
+
+    return $iResult;
   }
 
 ?>
