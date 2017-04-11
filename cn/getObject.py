baf7726fd53795a9e965279ddc62a99b02dbb5f5
Naomi Navkal <navkal@hotmail.com>
2017-04-10 21:56:26 -0400

    Prototype

---
+++ cn/getObject.py
@@ -0,0 +1,32 @@
+# Copyright 2017 Energize Apps.  All rights reserved.
+
+import printctl
+import argparse
+import json
+
+printctl.off()
+import queries
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser( description='retrieve object from Location Dictionary database' )
+    parser.add_argument( '-t', '--table', dest='table', help='object table' )
+    parser.add_argument( '-i', '--id', dest='id',  help='object id' )
+    parser.add_argument( '-p', '--path', dest='path',  help='object path' )
+    args = parser.parse_args()
+
+    if args.path:
+      selector = 'path="' + args.path + '"'
+    elif args.id:
+      selector = 'id=' + args.id
+    else:
+      selector = ''
+
+    try:
+      object = eval( 'queries.' + args.table + '( ' + selector + ' )' )
+    except:
+      dict = { 'Error': 'Could not retrieve [' + selector + '] from [' + args.table + '] table' }
+    else:
+      dict = object.__dict__;
+
+    printctl.on( )
+    print( json.dumps( dict ) )
