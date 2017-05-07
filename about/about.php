5871939c647fda7fe15427884fe383226d12ae01
Naomi Navkal <navkal@hotmail.com>
2017-05-06 21:51:21 -0400

    Add image files

--- about/about.php
+++ about/about.php
@@ -6,21 +6,6 @@
   </div>
 
   <br/>
-  <h4>Features</h4>
-  <dl class="dl-horizontal">
-    <dt>Circuits</dt>
-    <dd>TBD</dd>
-    <dt>Panels</dt>
-    <dd>TBD</dd>
-    <dt>Transformers</dt>
-    <dd>TBD</dd>
-    <dt>Devices</dt>
-    <dd>TBD</dd>
-    <dt>Locations</dt>
-    <dd>TBD</dd>
-  </dl>
-
-  <br/>
   <h4>Team</h4>
   <dl class="dl-horizontal">
     <dt>Anil Navkal</dt>
